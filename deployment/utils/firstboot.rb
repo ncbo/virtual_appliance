e38167c4b9c6e781bf4ccd0dbf95b6714facdbc9
Alex Skrenchuk <alexskr@stanford.edu>
2018-02-20 17:22:29 -0800

    typo fix

--- deployment/utils/firstboot.rb
+++ deployment/utils/firstboot.rb
@@ -6,7 +6,7 @@
 
 
 
-#dont run if this is not the first boot.
+# dont run if this is not the first boot.
 unless File.file?('/root/firstboot')
   abort ('doesnt look like this is the first time boot; aborting!')
 end
@@ -15,8 +15,8 @@ require_relative 'apikey.rb'
 
 CONFIG_FILE = '/srv/ncbo/virtual_appliance/appliance_config/site_config.rb'
 
-#reset_apikey('admin')
-#reset_apikey('appliance')
+reset_apikey('admin')
+reset_apikey('appliance')
 api_key = get_apikey('appliance')
 
 # update config files
@@ -26,7 +26,7 @@ new_content = text.gsub(/^\$API_KEY =.*$/, "\$API_KEY = \"#{api_key}\"")
 File.open(CONFIG_FILE, 'w') { |file| file.puts new_content }
 
 FileUtils.cp "#{CONFIG_FILE}", '/srv/rails/bioportal_web_ui/current/config'
-FileUtils.chown 'ontoportal', 'ontoportal' ", '/srv/rails/bioportal_web_ui/current/config'
+FileUtils.chown 'ontoportal', 'ontoportal', '/srv/rails/bioportal_web_ui/current/config'
 
-#restart ontoportal stack
+# restart ontoportal stack
 system "/usr/local/bin/bprestart"
