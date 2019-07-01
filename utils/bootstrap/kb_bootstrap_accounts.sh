cf35246bc0fbf878761fcae2eb01f6d723fef453
Alex Skrenchuk <alexskr@stanford.edu>
2019-07-01 14:58:54 -0700

    add biomixer service account

--- utils/bootstrap/kb_bootstrap_accounts.sh
+++ utils/bootstrap/kb_bootstrap_accounts.sh
@@ -5,3 +5,5 @@ cd /srv/ncbo/ncbo_cron
 bundle exec rake user:create['admin','admin@nodomain.org','changemeNOW']
 bundle exec rake user:adminify['admin']
 bundle exec rake user:create['ontoportal_ui','ontoportal_ui@nodomain.org']
+bundle exec rake user:create['biomixer','biomixer@nodomain.org']
+bundle exec rake user:apikey:reset['biomixer','efcfb6e1-bcf8-4a5d-a46a-3ae8867241a1']
