924a2d3bf93634dd93e51a450ebdcdabcbc6a739
Alex Skrenchuk <alexskr@stanford.edu>
2020-07-31 12:02:56 -0700

    add full path for maintenance file

--- utils/bootstrap/bootstrap.sh
+++ utils/bootstrap/bootstrap.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 sudo opstop
 sudo systemctl start 4s-boss
-./kb_bootstrap_create_kb.sh
+sh kb_bootstrap_create_kb.sh
 pushd /srv/ontoportal/virtual_appliance/deployment
 sh setup_deploy_env.sh
 sh deploy_all.sh
 sudo opstart
+sudo opstatus -v
 popd
+
+# set up maintanence page which will remain in palce untill firstboot.rb removes it.
+cp /srv/ontoportal/virtual_appliance/utils/bootstrap/maintenance.html /srv/ontoportal/bioportal_web_ui/current/public/system
+
 ./kb_bootstrap_accounts.sh
 ruby ../bioportal_ontologies_import.rb
+
 pushd /srv/ontoportal/ncbo_cron
 bin/ncbo_ontology_process -o STY
-# set up maintanence page which will remain in palce untill firstboot.rb removes it.
-cp maintenance.html /srv/ontoportal/bioportal_web_ui/current/public/system/maintenance.html
 popd
