0a0902a12a29606e9ec10d82a0e93b8c106d5d1b
Alex Skrenchuk <alexskr@stanford.edu>
2020-07-31 17:12:06 -0700

    add sample config for google analytics

--- utils/bootstrap/kb_bootstrap_create_kb.sh
+++ utils/bootstrap/kb_bootstrap_create_kb.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Create 4store KB for ontologies api with 4 segments
 
+echo "---> Create 4store KB"
 /usr/local/bin/4s-admin delete-stores ontologies_api
 /usr/local/bin/4s-admin create-store --segments=4 ontologies_api
 /usr/local/bin/4s-admin start-stores ontologies_api
+/usr/local/bin/4s-admin list-stores
+echo "---> DONE creating 4store KB"
