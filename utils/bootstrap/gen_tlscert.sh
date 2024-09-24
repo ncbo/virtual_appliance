8835560bd8a70579623829d8a3e4238cca660eaa
Alex Skrenchuk <alexskr@ontoportal-appliance-v4.stanford.edu>
2024-09-24 11:30:17 -0700

    add ubuntu support

--- utils/bootstrap/gen_tlscert.sh
+++ utils/bootstrap/gen_tlscert.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # generate self-signed dummy tls certificate
 
+# Detect the operating system
+if [ -f /etc/debian_version ]; then
+    OS="Debian"
+elif [ -f /etc/redhat-release ]; then
+    OS="RedHat"
+else
+    echo "Unsupported OS. Only Debian-based or RedHat-based systems are supported."
+    exit 1
+fi
+
+
+if [ "$OS" == "RedHat" ]; then
 FQDN=`hostname`
 head /dev/urandom > /dev/null
 openssl req -new -newkey rsa:2048 -rand /dev/urandom -nodes -x509 \
@@ -10,3 +22,10 @@ openssl req -new -newkey rsa:2048 -rand /dev/urandom -nodes -x509 \
 
 chmod 600 /etc/pki/tls/private/localhost.key
 chmod 600 /etc/ssl/certs/localhost.crt
+fi
+
+if [ "$OS" == "Debian" ]; then
+  make-ssl-cert generate-default-snakeoil --force-overwrite
+fi
+
+echo "Created self-signed TLS certs"
