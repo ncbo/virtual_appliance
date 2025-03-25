d97a1beadbedd54c45c9b32a86517574b545a9a8
Alex Skrenchuk <alexskr@stanford.edu>
2025-03-25 06:26:33 +0000

    use puppet's embeded ruby

--- utils/infra/install_puppet.sh
+++ utils/infra/install_puppet.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 #
 # A simple script to install Puppet 7 on Ubuntu 22.04 (Jammy) from the Puppet repository
-# and then clone OnotPortal control-repo from GitHub.
+# and then clone OntoPortal control-repo from GitHub.
 
 if [[ $EUID -ne 0 ]]; then
   echo "Please run this script as root"
   exit 1
 fi
+
+# Set environment variables so apt/dpkg won't prompt for input:
+#  - DEBIAN_FRONTEND=noninteractive prevents package configuration dialogs.
+#  - NEEDRESTART_SUSPEND=1 prevents needrestart from prompting to restart services.
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_SUSPEND=1
 
 echo "==> Adding Puppet 7 repository..."
 # Download and install Puppet's release package for Ubuntu 22.04 (Jammy)
@@ -14,16 +20,15 @@ wget -q -O /tmp/puppet7-release-jammy.deb https://apt.puppet.com/puppet7-release
 
 dpkg -i /tmp/puppet7-release-jammy.deb
 rm -f /tmp/puppet7-release-jammy.deb
-apt-get update -y
+apt-get update
 apt-get install -y puppet-agent
 
 echo "==> Cloning the control-repo from GitHub..."
-# Replace with your actual GitHub URL and desired path
 git clone https://github.com/ontoportal/ontoportal-appliance-puppet-control-repo /etc/puppetlabs/code/environments/production
 
 cd /etc/puppetlabs/code/environments/production
-bundle install
-r10k puppetfile install -v
+
+/opt/puppetlabs/puppet/bin/gem install r10k -v '~> 3.16'
+/opt/puppetlabs/puppet/bin/r10k puppetfile install -v
 
 echo "==> Puppet 7 installation and control-repo clone completed."
-
