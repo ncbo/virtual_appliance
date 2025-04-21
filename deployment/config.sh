652e163f394f3b0063abc2655368c27af77fc5c7
Alex Skrenchuk <alexskr@stanford.edu>
2025-04-20 23:08:12 -0700

    update paths

--- deployment/config.sh
+++ deployment/config.sh
@@ -10,20 +10,28 @@
   exit 1
 }
 
-# Load component versions
-source "$(dirname "$0")/versions"
-
-# Check for correct user
-if [[ "$USER" != 'ontoportal' ]]; then
-  echo "ERROR: You need to run this script as the 'ontoportal' user"
-  return 1
-fi
-
 # General settings
-export DATA_DIR="/srv/ontoportal/data"
+export DATA_DIR="/srv/ontoportal"
 export APP_DIR="/opt/ontoportal"
 export VIRTUAL_APPLIANCE_REPO="/opt/ontoportal/virtual_appliance"
 export BUNDLE_PATH="/opt/ontoportal/.bundle"
+export ADMIN_USER="op-admin"
+
+source "$(dirname "$0")/versions"
+
+# Check for correct user
+if [[ "$USER" != $ADMIN_USER ]]; then
+  echo "ERROR: You need to run this script as the $ADMIN_USER user"
+  exit 1
+fi
+
+# General settings
+export DATA_DIR="/srv/ontoportal"
+export APP_DIR="/opt/ontoportal"
+export VIRTUAL_APPLIANCE_REPO="/opt/ontoportal/virtual_appliance"
+export BUNDLE_PATH="/opt/ontoportal/.bundle"
+export ADMIN_USER="op-admin"
+
 # export LOCAL_CONFIG_PATH="${VIRTUAL_APPLIANCE_REPO}/appliance_config"
 
 # GitHub settings
