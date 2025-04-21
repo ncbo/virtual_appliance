ca3b30b0e5b5c93e91b32e7e6423fc5ffe61ddc6
Alex Skrenchuk <alexskr@stanford.edu>
2025-04-20 23:09:33 -0700

    use opctl, update paths

--- utils/bootstrap/kb_bootstrap_AG.sh
+++ utils/bootstrap/kb_bootstrap_AG.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #  !!!!!!!! AG licence is required !!!!!!!
 
-sudo opstop
+sudo opctl stop
 sudo service agraph start
 ./bootstrap_create_AG_repository.sh
-sudo opstart
+sudo opctl start 
 ./kb_bootstrap_accounts.sh
 ruby ../bioportal_ontologies_import.rb
-pushd /srv/ontoportal/ncbo_cron
+pushd /opt/ontoportal/ncbo_cron
 bin/ncbo_ontology_process -o STY
 popd
